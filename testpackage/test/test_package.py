358903ed1168a2646486ddd3c0be0ad3b21d290f
Sohaib Qureshi <sohaib_qureshi@apple.com>
2017-08-08 14:04:31 -0700

    Pushing to test

--- testpackage/test/test_package.py
+++ testpackage/test/test_package.py
@@ -5,3 +5,7 @@ class PackageTest(unittest.TestCase):
 
 	def test_packit(self):
 		self.assertEqual(7, testpackage.package.packit(3,4))
+
+	def test_failing_packit(self):
+		self.assertEqual(19, testpackage.package.packit(3,4))
+
